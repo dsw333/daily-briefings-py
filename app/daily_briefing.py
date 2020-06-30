78874dbaa47531775e892ad46b6d2655e74a463c
dsw333 <65722424+dsw333@users.noreply.github.com>
2020-06-29 21:58:46 -0400

    completed code with placeholders
    placeholders for twilio phone numbers. Not cleaned up

--- app/daily_briefing.py
+++ app/daily_briefing.py
@@ -3,7 +3,7 @@
 import os
 from dotenv import load_dotenv
 from datetime import date
-#from pprint import pprint
+#from print import pprint
 
 from app import APP_ENV
 from app.weather_service import get_hourly_forecasts
@@ -11,28 +11,43 @@ from app.sms_service import send_sms
 
 load_dotenv()
 
-MY_NAME = os.getenv("MY_NAME", default="Player 1")
 
-if __name__ == "__main__":
+#If weather is greater than 75% then send a text message that you need sunblock
 
-    if APP_ENV == "development":
-        zip_code = input("PLEASE INPUT A ZIP CODE (e.g. 06510): ")
-        weather_results = get_hourly_forecasts(zip_code=zip_code) # invoke with custom params
-    else:
-        weather_results = get_hourly_forecasts() # invoke with default params
+
+if 'my_temperature_f' > 75:
+    send_sms(print("Put on sunscreen"))
+
+
+
+
+
+
+
+
+#
+#MY_NAME = os.getenv("MY_NAME", default="Player 1")
+
+#if __name__ == "__main__":
+
+ #   if APP_ENV == "development":
+  #      zip_code = input("PLEASE INPUT A ZIP CODE (e.g. 06510): ")
+   #     weather_results = get_hourly_forecasts(zip_code=zip_code) # invoke with custom params
+  #  else:
+  #      weather_results = get_hourly_forecasts() # invoke with default params
 
     #print(weather_results)
 
-    html = ""
-    html += f"<h3>Good Morning, {MY_NAME}!</h3>"
+#    html = ""
+ #   html += f"<h3>Good Morning, {MY_NAME}!</h3>"
 
-    html += "<h4>Today's Date</h4>"
-    html += f"<p>{date.today().strftime('%A, %B %d, %Y')}</p>"
+#    html += "<h4>Today's Date</h4>"
+ #   html += f"<p>{date.today().strftime('%A, %B %d, %Y')}</p>"
 
-    html += f"<h4>Weather Forecast for {weather_results['city_name'].title()}</h4>"
-    html += "<ul>"
-    for hourly in weather_results["hourly_forecasts"]:
-        html += f"<li>{hourly['timestamp']} | {hourly['temp']} | {hourly['conditions'].upper()}</li>"
-    html += "</ul>"
+#    html += f"<h4>Weather Forecast for {weather_results['city_name'].title()}</h4>"
+#    html += "<ul>"
+#    for hourly in weather_results["hourly_forecasts"]:
+ #       html += f"<li>{hourly['timestamp']} | {hourly['temp']} | {hourly['conditions'].upper()}</li>"
+ #   html += "</ul>"
 
-    send_email(subject="[Daily Briefing] My Morning Report", html=html)
+  #  send_email(subject="[Daily Briefing] My Morning Report", html=html)
--- app/sms_service.py
+++ app/sms_service.py
@@ -4,16 +4,18 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'ACXXXXXXXX'
-auth_token = 'acXXXXXXXXX'
+account_sid = 'ACXXXXXXXXXXXXXXXX'
+auth_token = 'acXXXXXXXXXXXXXXXXXXXXX'
 client = Client(account_sid, auth_token)
 
 message = client.messages \
                 .create(
                      body="Don't forget sunblock today",
                      from_='+12058272450',
-                     to='+17324047720'
+                     to='+XXXXXXXXXX'
                  )
 
 print(message.sid)
 
+
+#pip intall twilio
